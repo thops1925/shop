51f51eca7ba30108dca63f2e5e8b8e0a294dcac7
ThopsD <74445064+thops1925@users.noreply.github.com>
2021-12-08 13:19:46 +0800

    fix usestate to 0

--- src/components/CheckOutForm/CheckOut/CheckOut.jsx
+++ src/components/CheckOutForm/CheckOut/CheckOut.jsx
@@ -15,7 +15,7 @@ const steps = ['Shipping address', 'Payment details'];
 
 const CheckOut = () => {
   const classes = useStyles();
-  const [activeStep, setActiveStep] = useState(0);
+  //   const [activeStep, setActiveStep] = useState(0);
 
   //   const Form = () =>
   //     activeStep === 0 ? <ShippingAddress /> : <PaymentDetails />;
@@ -27,7 +27,7 @@ const CheckOut = () => {
           <Typography variant="h4" align="center">
             Checkout
           </Typography>
-          <Stepper activeStep={activeStep} className={classes.stepper}>
+          <Stepper activeStep={0} className={classes.stepper}>
             {steps.map((step) => (
               <Step key={step}>
                 <StepLabel>{step}</StepLabel>
